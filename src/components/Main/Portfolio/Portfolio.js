31979520a6d39450f89022d52828ca14cd354694
Olga Pomytkina <olpoma@yandex.ru>
2023-07-11 19:19:35 +0700

    feat: portfolio

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import link from '../../../img/link-min.png';
 import './Portfolio.css';
 
 function Portfolio() {
-  return (
-    <section className="portfolio">
-      <h4 className="portfolio__title">Портфолио</h4>
-      <a href="https://mariagrom.github.io/how-to-learn/" className="portfolio__link" target="_blank" rel="noreferrer">
-        <p  className="portfolio__subtitle">Статичный сайт</p>
-        <img className="portfolio__img" src={link} alt="Ссылка" />
-      </a>
-      <a href="https://mariagrom.github.io/russian-travel/" className="portfolio__link" target="_blank" rel="noreferrer">
-        <p className="portfolio__subtitle">Адаптивный сайт</p>
-        <img className="portfolio__img" src={link} alt="Ссылка" />
-      </a>
-      <a href="https://mariagrom.github.io/mesto/" className="portfolio__link" target="_blank" rel="noreferrer">
-        <p className="portfolio__subtitle">Одностраничное приложение</p>
-        <img className="portfolio__img" src={link} alt="Ссылка" />
-      </a>
-    </section>
-  )
+    return (
+        <section className='portfolio'>
+            <div className='portfolio__section'>
+                <h4 className='portfolio__title'>Портфолио</h4>
+                <a href='https://olpom.github.io/how-to-learn/' className='portfolio__link' target='_blank' rel='noreferrer'>
+                    <p className='portfolio__subtitle'>Статичный сайт</p>
+                    <p className='portfolio__arrow'>↗</p>
+                </a>
+                <a href='https://olpom.github.io/russian-travel/' className='portfolio__link' target='_blank' rel='noreferrer'>
+                    <p className='portfolio__subtitle'>Адаптивный сайт</p>
+                    <p className='portfolio__arrow'>↗</p>
+                </a>
+                <a href='olpom.github.io/react-mesto-auth/' className='portfolio__link' target='_blank' rel='noreferrer'>
+                    <p className='portfolio__subtitle'>Одностраничное приложение</p>
+                    <p className='portfolio__arrow'>↗</p>
+                </a>
+            </div>
+        </section>
+    )
 }
 
-export default Portfolio
+export default Portfolio;
